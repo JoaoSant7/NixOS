88dc038ed6d00f3546b38a4cd1f2f445b4eacbb5
JoaoSant7 <pedro_romano7@proton.me>
2026-06-26 11:47:03 -0300

    Initial commit: add flake.nix; hosts/nixos

---
+++ flake.nix
@@ -0,0 +1,17 @@
+{
+  description = "Backup NixOS";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-26.05";
+  };
+
+  outputs = { self, nixpkgs, ... }: {
+    nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
+      system = "x86_64-linux";
+
+      modules = [
+        ./hosts/nixos/configuration.nix
+      ];
+    };
+  };
+}
