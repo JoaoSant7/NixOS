705a1938627f5edd7f2669bc60de047f39459a28
JoaoSant7 <pedro_romano7@proton.me>
2026-06-29 12:34:54 -0300

    fix: add noctalia overlays on flake.nix

--- flake.nix
+++ flake.nix
@@ -2,15 +2,14 @@
   description = "Backup NixOS";
 
   inputs = {
-  nixpkgs.url = "github:NixOS/nixpkgs/nixos-26.05";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-26.05";
+    nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
 
-  nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
-
-  noctalia = {
-    url = "github:noctalia-dev/noctalia/legacy-v4";
-    inputs.nixpkgs.follows = "nixpkgs-unstable";
+    noctalia = {
+      url = "github:noctalia-dev/noctalia/legacy-v4";
+      inputs.nixpkgs.follows = "nixpkgs-unstable"; # This is great since Noctalia targets unstable
+    };
   };
-};
 
   outputs = inputs@{ self, nixpkgs, noctalia, ... }: {
     nixosConfigurations = {
@@ -18,7 +17,6 @@
       nixos = nixpkgs.lib.nixosSystem {
         system = "x86_64-linux";
         specialArgs = { inherit inputs; };
-
         modules = [
           ./hosts/nixos/configuration.nix
         ];
@@ -30,6 +28,11 @@
 
         modules = [
           ./hosts/hyprnix/configuration.nix
+          
+          # 1. Bring in Noctalia's NixOS module features
+          noctalia.nixosModules.default 
+
+          # 2. Keep your overlay so the packages build with the right nixpkgs channel
           { nixpkgs.overlays = [ noctalia.overlays.default ]; }
         ];
       };
