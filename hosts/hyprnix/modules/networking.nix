8f73361d280b1d519b5355c153fcb8fc89474bd0
JoaoSant7 <pedro_romano7@proton.me>
2026-06-29 18:59:15 -0300

    change: hostname of hyprnix

--- hosts/hyprnix/modules/networking.nix
+++ hosts/hyprnix/modules/networking.nix
@@ -1,7 +1,7 @@
 { config, pkgs, lib, ... }:
 
 {
-  networking.hostName = "nixos"; # Define your hostname.
+  networking.hostName = "hyprnix"; # Define your hostname.
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
   # Configure network proxy if necessary
   # networking.proxy.default = "http://user:password@proxy:port/";
