a194dddbcb895fa40279311558dc2ab457a65522
JoaoSant7 <pedro_romano7@proton.me>
2026-07-01 20:54:39 -0300

    add: more extraportals for compatibility with gtk and kde apps

--- hosts/hyprnix/modules/hyprland.nix
+++ hosts/hyprnix/modules/hyprland.nix
@@ -45,7 +45,17 @@ in
   # Screensharing
   xdg.portal = {
     enable = true;
-    extraPortals = with pkgs; [ xdg-desktop-portal-hyprland ];
+    extraPortals = with pkgs; [
+      xdg-desktop-portal-gtk
+      xdg-desktop-portal-hyprland
+      kdePackages.xdg-desktop-portal-kde
+    ];
+    config = {
+      common.default = [ "hyprland" "gtk" ];
+      hyprland.default = [ "hyprland" "gtk" ];
+      hyprland."org.freedesktop.impl.portal.FileChooser" = [ "kde" ];
+    };
+    xdgOpenUsePortal = false;
   };
 
   # Configure keymap.
