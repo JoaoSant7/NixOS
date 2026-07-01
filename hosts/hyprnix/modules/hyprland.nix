8777007e7ee68fffc1fc7a8f88a69cfe43dfd3a5
JoaoSant7 <pedro_romano7@proton.me>
2026-07-01 20:45:56 -0300

    unify: sddm with hyprland

--- hosts/hyprnix/modules/hyprland.nix
+++ hosts/hyprnix/modules/hyprland.nix
@@ -1,5 +1,22 @@
-{ config, pkgs, lib, ... }:
+{ pkgs, ... }:
 
+let
+  sddm-astronaut = (pkgs.sddm-astronaut.override {
+    embeddedTheme = "astronaut"; # troque pelo tema que preferir
+    themeConfig = {
+      HeaderTextColor = "#d5c4a1";
+      Background = "Backgrounds/your-custom-background.png";
+      # ... outras opções de tema
+    };
+  }).overrideAttrs (oldAttrs: {
+    # Injeta seu wallpaper customizado no tema
+    installPhase = oldAttrs.installPhase + ''
+      chmod u+w $out/share/sddm/themes/sddm-astronaut-theme/Backgrounds/
+      cp ${../../../nix-wallpapers/catholicon-holy-sepulchre.jpg} \
+        $out/share/sddm/themes/sddm-astronaut-theme/Backgrounds/your-custom-background.png
+    '';
+  });
+in
 {
   # Enable Hyprland with UWSM for proper session registration.
   programs.hyprland = {
@@ -7,19 +24,28 @@
     withUWSM = true;
     xwayland.enable = true;
   };
-
   # Enable UWSM.
   programs.uwsm = {
     enable = true;
+  };
 
-    # Uncomment this if you need a custom compositor entry.
-    # waylandCompositors = {
-    #   hyprland = {
-    #     prettyName = "Hyprland";
-    #     comment = "Hyprland compositor managed by UWSM";
-    #     binPath = "/run/current-system/sw/bin/uwsm-start-hyprland";
-    #   };
-    # };
+  # SDDM
+  environment.systemPackages = [ sddm-astronaut ];
+
+  services.displayManager.sddm = {
+    enable = true;
+    wayland.enable = true;
+    package = pkgs.kdePackages.sddm;
+    extraPackages = with pkgs; [
+      kdePackages.qtmultimedia # necessário para vídeos/áudio de fundo
+    ];
+    theme = "sddm-astronaut-theme";
+  };
+
+  # Screensharing
+  xdg.portal = {
+    enable = true;
+    extraPortals = with pkgs; [ xdg-desktop-portal-hyprland ];
   };
 
   # Configure keymap.
@@ -32,7 +58,6 @@
   i18n.inputMethod = {
     enable = true;
     type = "fcitx5";
-
     fcitx5 = {
       waylandFrontend = true;
       addons = with pkgs; [
@@ -40,5 +65,4 @@
       ];
     };
   };
-
 }
