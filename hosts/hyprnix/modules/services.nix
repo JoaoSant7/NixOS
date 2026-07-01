5058872e3913a8286f91ed3dbb2148a3faefd207
JoaoSant7 <pedro_romano7@proton.me>
2026-06-30 21:56:48 -0300

    add: udev and printing to services module

--- hosts/hyprnix/modules/services.nix
+++ hosts/hyprnix/modules/services.nix
@@ -3,4 +3,15 @@
 {
   hardware.bluetooth.enable = true;
   services.power-profiles-daemon.enable = true;
+
+  services.udev.enable = true;
+
+  services.udev.packages = with pkgs; [
+    vial
+    via
+  ];
+
+  # Enable printing.
+  services.printing.enable = true;
+
 }
