30bae6f3cdcf34596eea2e665bfc0f6e2c91c30c
JoaoSant7 <pedro_romano7@proton.me>
2026-06-30 23:55:32 -0300

    change: add noctalia modules settings for noctalia module and cachix fallback

--- hosts/hyprnix/modules/noctalia.nix
+++ hosts/hyprnix/modules/noctalia.nix
@@ -1,8 +1,14 @@
 { pkgs, inputs, ... }:
 
 {
-  environment.systemPackages = [
-    inputs.noctalia.packages.${pkgs.stdenv.hostPlatform.system}.default
-  ];
+  imports = [ inputs.noctalia.nixosModules.default ];
 
+  nixpkgs.overlays = [ inputs.noctalia.overlays.default ];
+
+  services.noctalia-shell.enable = true;
+
+  nix.settings = {
+    substituters = [ "https://noctalia.cachix.org" ];
+    trusted-public-keys = [ "noctalia.cachix.org-1:pCOR47nnMEo5thcxNDtzWpOxNFQsBRglJzxWPp3dkU4=" ];
+  };
 }
