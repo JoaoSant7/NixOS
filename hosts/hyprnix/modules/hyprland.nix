e3d5a1476ea5abfc809c7ef7a1a9a130b8bc4388
JoaoSant7 <pedro_romano7@proton.me>
2026-07-01 20:50:33 -0300

    add: cachix cachy for hyprland

--- hosts/hyprnix/modules/hyprland.nix
+++ hosts/hyprnix/modules/hyprland.nix
@@ -65,4 +65,10 @@ in
       ];
     };
   };
+
+  # Add cachix cache.
+  nix.settings = {
+    substituters = [ "https://hyprland.cachix.org" ];
+    trusted-public-keys = [ "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc=" ];
+  };
 }
