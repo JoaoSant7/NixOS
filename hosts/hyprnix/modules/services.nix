6ea250feb9d8c3f992e7bd98f70486746c7a812b
JoaoSant7 <pedro_romano7@proton.me>
2026-07-01 23:47:46 -0300

    move: add more services to the module

--- hosts/hyprnix/modules/services.nix
+++ hosts/hyprnix/modules/services.nix
@@ -1,16 +1,60 @@
-{ config, pkgs, lib, ... }:
+{ pkgs, ... }:
 
 {
   hardware.bluetooth.enable = true;
+
+  # Enable the OpenSSH daemon.
+  services.openssh.enable = true;
+
   services.power-profiles-daemon.enable = true;
 
-  services.udev.enable = true;
+  services.gvfs.enable = true;
 
+  # udev services.
+  services.udev.enable = true;
   services.udev.packages = with pkgs; [
     vial
     via
   ];
 
+  # Flatpaks.
+  services.flatpak = {
+    enable = true;
+  };
+
+  #Syncthing
+  services.syncthing = {
+    enable = true;
+    openDefaultPorts = true;
+    user = "pedro";
+    group = "users";
+    configDir = "/home/pedro/.config/syncthing";
+  };
+
+  #XDG Portals.
+  xdg.portal = {
+    enable = true;
+    extraPortals = with pkgs; [
+      xdg-desktop-portal-gtk
+      xdg-desktop-portal-hyprland
+      kdePackages.xdg-desktop-portal-kde
+    ];
+    config = {
+      common.default = [ "hyprland" "gtk" ];
+      hyprland.default = [ "hyprland" "gtk" ];
+      hyprland."org.freedesktop.impl.portal.FileChooser" = [ "kde" ];
+    };
+    xdgOpenUsePortal = false;
+  };
+
+
   # Enable printing.
   services.printing.enable = true;
+
+  # Enable nix garbage collection.
+  nix.gc = {
+    automatic = true;
+    dates = "weekly";
+    options = "--delete-older-than 15d";
+  };
 }
