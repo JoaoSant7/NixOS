8b35edd497600ced71914a61f352fe20df7fe0a7
JoaoSant7 <pedro_romano7@proton.me>
2026-07-01 23:47:16 -0300

    move: xdg portals to services module

--- hosts/hyprnix/modules/hyprland.nix
+++ hosts/hyprnix/modules/hyprland.nix
@@ -42,22 +42,6 @@ in
     theme = "sddm-astronaut-theme";
   };
 
-  # Screensharing
-  xdg.portal = {
-    enable = true;
-    extraPortals = with pkgs; [
-      xdg-desktop-portal-gtk
-      xdg-desktop-portal-hyprland
-      kdePackages.xdg-desktop-portal-kde
-    ];
-    config = {
-      common.default = [ "hyprland" "gtk" ];
-      hyprland.default = [ "hyprland" "gtk" ];
-      hyprland."org.freedesktop.impl.portal.FileChooser" = [ "kde" ];
-    };
-    xdgOpenUsePortal = false;
-  };
-
   # Configure keymap.
   services.xserver.xkb = {
     layout = "us";
