91c92c602c975b0c736b4fa45d15a99a90540264
JoaoSant7 <pedro_romano7@proton.me>
2026-07-02 22:52:05 -0300

    add: sddm-astronaut to niri

--- hosts/hyprnix/modules/niri.nix
+++ hosts/hyprnix/modules/niri.nix
@@ -1,7 +1,23 @@
-{ config, pkgs, lib, ... }:
+{ pkgs, ... }:
 
+let
+  sddm-astronaut = (pkgs.sddm-astronaut.override {
+    embeddedTheme = "astronaut"; # troque pelo tema que preferir
+    themeConfig = {
+      HeaderTextColor = "#d5c4a1";
+      Background = "Backgrounds/your-custom-background.png";
+      # ... outras opções de tema
+    };
+  }).overrideAttrs (oldAttrs: {
+    # Injeta seu wallpaper customizado no tema
+    installPhase = oldAttrs.installPhase + ''
+      chmod u+w $out/share/sddm/themes/sddm-astronaut-theme/Backgrounds/
+      cp ${../../../nix-wallpapers/catholicon-holy-sepulchre.jpg} \
+        $out/share/sddm/themes/sddm-astronaut-theme/Backgrounds/your-custom-background.png
+    '';
+  });
+in
 {
-
   # Enable Niri with UWSM for proper session registration.
   programs.niri = {
     enable = true;
@@ -11,6 +27,9 @@
   programs.uwsm = {
     enable = true;
   };
+
+  # SDDM
+  environment.systemPackages = [ sddm-astronaut ];
 
   # Configure keymap.
   services.xserver.xkb = {
@@ -22,7 +41,6 @@
   i18n.inputMethod = {
     enable = true;
     type = "fcitx5";
-
     fcitx5 = {
       waylandFrontend = true;
       addons = with pkgs; [
