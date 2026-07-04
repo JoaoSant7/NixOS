be1e6569e55a27b25fdb5fedf3e1134a9559132a
JoaoSant7 <pedro_romano7@proton.me>
2026-07-04 20:38:00 -0300

    add: astronaut pkg to extra pkgs on services module

--- hosts/hyprnix/modules/services.nix
+++ hosts/hyprnix/modules/services.nix
@@ -37,12 +37,12 @@
   };
 
   #SDDM
-
   services.displayManager.sddm = {
     enable = true;
     wayland.enable = true;
     package = pkgs.kdePackages.sddm;
     extraPackages = with pkgs; [
+      pkgs.sddm-astronaut
       kdePackages.qtmultimedia # necessário para vídeos/áudio de fundo
     ];
     theme = "sddm-astronaut-theme";
