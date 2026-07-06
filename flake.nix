6fb8477ba3e3f576d9cf35571e4ddecab9fb9389
JoaoSant7 <pedro_romano7@proton.me>
2026-07-06 00:38:07 -0300

    add: home-manager module to flake.nix, plus the noctalia home-manager module

--- flake.nix
+++ flake.nix
@@ -1,38 +1,34 @@
 {
   description = "Backup NixOS";
-
   nixConfig = {
     extra-substituters = [ "https://noctalia.cachix.org" ];
     extra-trusted-public-keys = [ "noctalia.cachix.org-1:pCOR47nnMEo5thcxNDtzWpOxNFQsBRglJzxWPp3dkU4=" ];
   };
-
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-26.05";
     nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
-
     nix-flatpak = {
       url = "github:gmodena/nix-flatpak?ref=latest";
     };
-
     noctalia = {
-      url = "github:noctalia-dev/noctalia/legacy-v4";
-      inputs.nixpkgs.follows = "nixpkgs-unstable";
+      url = "github:noctalia-dev/noctalia/cachix";
+    };
+    home-manager = {
+      url = "github:nix-community/home-manager/release-26.05";
+      inputs.nixpkgs.follows = "nixpkgs";
     };
   };
-
-  outputs = inputs@{ self, nixpkgs, noctalia, nix-flatpak, ... }: {
+  outputs = inputs@{ self, nixpkgs, noctalia, nix-flatpak, home-manager, ... }: {
     nixosConfigurations = {
-
       hyprnix = nixpkgs.lib.nixosSystem {
         system = "x86_64-linux";
         specialArgs = { inherit inputs; };
-
         modules = [
           nix-flatpak.nixosModules.nix-flatpak
+          home-manager.nixosModules.home-manager
           ./hosts/hyprnix/configuration.nix
         ];
       };
-
     };
   };
 }
