f7d3c6d8c6d9ea1bead0983546e356328a32a118
JoaoSant7 <pedro_romano7@proton.me>
2026-07-08 13:28:39 -0300

    move: noctalia cachix setting to noctalia module, and add noctalia cachix url to flake.nix

--- flake.nix
+++ flake.nix
@@ -1,23 +1,23 @@
 {
   description = "Backup NixOS";
-  nixConfig = {
-    extra-substituters = [ "https://noctalia.cachix.org" ];
-    extra-trusted-public-keys = [ "noctalia.cachix.org-1:pCOR47nnMEo5thcxNDtzWpOxNFQsBRglJzxWPp3dkU4=" ];
-  };
+
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-26.05";
     nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
     nix-flatpak = {
       url = "github:gmodena/nix-flatpak?ref=latest";
     };
+
     noctalia = {
       url = "github:noctalia-dev/noctalia/cachix";
     };
+
     home-manager = {
       url = "github:nix-community/home-manager/release-26.05";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
+
   outputs = inputs@{ self, nixpkgs, noctalia, nix-flatpak, home-manager, ... }: {
     nixosConfigurations = {
       hyprnix = nixpkgs.lib.nixosSystem {
--- hosts/hyprnix/modules/noctalia.nix
+++ hosts/hyprnix/modules/noctalia.nix
@@ -10,4 +10,8 @@
     recommendedServices.enable = true;
   };
 
+  nix.settings = {
+    substituters = [ "https://noctalia.cachix.org" ];
+    trusted-public-keys = [ "noctalia.cachix.org-1:pCOR47nnMEo5thcxNDtzWpOxNFQsBRglJzxWPp3dkU4=" ];
+  };
 }
