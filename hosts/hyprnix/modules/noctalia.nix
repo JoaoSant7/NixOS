ffa0827a9cd4059c9c3e5bfc32de0856059af684
JoaoSant7 <pedro_romano7@proton.me>
2026-07-08 13:20:29 -0300

    remove: cachix from noctalia module
    cachix is already declared on configuration.nix

--- hosts/hyprnix/modules/noctalia.nix
+++ hosts/hyprnix/modules/noctalia.nix
@@ -10,8 +10,4 @@
     recommendedServices.enable = true;
   };
 
-  nix.settings = {
-    substituters = [ "https://noctalia.cachix.org" ];
-    trusted-public-keys = [ "noctalia.cachix.org-1:pCOR47nnMEo5thcxNDtzWpOxNFQsBRglJzxWPp3dkU4=" ];
-  };
 }
