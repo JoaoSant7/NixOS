5d465849b165dbe2bab6ead1b06220a061a273c0
JoaoSant7 <pedro_romano7@proton.me>
2026-07-13 21:19:00 -0300

    update flake lock

--- hosts/hyprnix/modules/networking.nix
+++ hosts/hyprnix/modules/networking.nix
@@ -6,7 +6,8 @@
   # Configure network proxy if necessary
   # networking.proxy.default = "http://user:password@proxy:port/";
   # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
-  # Enable networking.
+  # Enable networking
+
   networking.networkmanager.enable = true;
 
   # Open ports in the firewall.
