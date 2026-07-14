c9fbdf774e9469af349d156e47399eb6321cd563
JoaoSant7 <pedro_romano7@proton.me>
2026-07-14 19:32:02 -0300

    remove hyprdynamicmonitors from flake.nix

--- flake.nix
+++ flake.nix
@@ -4,6 +4,7 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-26.05";
     nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
+
     nix-flatpak = {
       url = "github:gmodena/nix-flatpak?ref=latest";
     };
@@ -16,19 +17,31 @@
       url = "github:nix-community/home-manager/release-26.05";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+
   };
 
-  outputs = inputs@{ self, nixpkgs, noctalia, nix-flatpak, home-manager, ... }: {
-    nixosConfigurations = {
-      hyprnix = nixpkgs.lib.nixosSystem {
-        system = "x86_64-linux";
-        specialArgs = { inherit inputs; };
-        modules = [
-          nix-flatpak.nixosModules.nix-flatpak
-          home-manager.nixosModules.home-manager
-          ./hosts/hyprnix/configuration.nix
-        ];
+  outputs =
+    inputs@{ self
+    , nixpkgs
+    , noctalia
+    , nix-flatpak
+    , home-manager
+    , ...
+    }: {
+      nixosConfigurations = {
+        hyprnix = nixpkgs.lib.nixosSystem {
+          system = "x86_64-linux";
+
+          specialArgs = {
+            inherit inputs;
+          };
+
+          modules = [
+            nix-flatpak.nixosModules.nix-flatpak
+            home-manager.nixosModules.home-manager
+            ./hosts/hyprnix/configuration.nix
+          ];
+        };
       };
     };
-  };
 }
