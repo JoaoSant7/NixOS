1294d4a03cd4f8c1616a3955cd856693e6dc4081
JoaoSant7 <pedro_romano7@proton.me>
2026-07-18 00:04:32 -0300

    create hardware folder with the respective modules

---
+++ hosts/hyprnix/modules/hardware/nvidia.nix
@@ -0,0 +1,25 @@
+{ config, pkgs, lib, ... }:
+
+{
+
+  # Load nvidia modules early in initrd
+  boot.initrd.kernelModules = [ "nvidia" "nvidia_modeset" "nvidia_uvm" "nvidia_drm" ];
+
+  hardware.graphics = {
+    enable = true;
+    enable32Bit = true;
+  };
+
+  services.xserver.videoDrivers = [ "nvidia" ];
+
+  hardware.nvidia = {
+    open = true;
+    modesetting.enable = true;
+    nvidiaSettings = true;
+    package = config.boot.kernelPackages.nvidiaPackages.stable;
+    powerManagement.enable = true;
+    powerManagement.finegrained = false;
+  };
+
+  boot.kernelParams = [ "nvidia.NVreg_TemporaryFilePath=/var/tmp" ];
+}
