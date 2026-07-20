9dc2cf55daa7126b03c8fb16edf45a6c665d98ca
JoaoSant7 <pedro_romano7@proton.me>
2026-07-19 21:47:48 -0300

    remove qogir and adwaita icons; also remove sddm astronaut

--- hosts/hyprnix/modules/pkgs/theming.nix
+++ hosts/hyprnix/modules/pkgs/theming.nix
@@ -2,12 +2,9 @@
 
 {
   environment.systemPackages = with pkgs; [
-    adwaita-icon-theme
     adw-gtk3
     bibata-cursors
     papirus-nord
-    qogir-icon-theme
-    sddm-astronaut
     sound-theme-freedesktop
     tela-icon-theme
     kdePackages.qt6ct
