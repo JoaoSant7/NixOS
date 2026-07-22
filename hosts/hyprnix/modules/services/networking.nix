f004602af807900451995513bf4ac4717e2f511c
JoaoSant7 <pedro_romano7@proton.me>
2026-07-22 12:18:15 -0300

    add nextdns

--- hosts/hyprnix/modules/services/networking.nix
+++ hosts/hyprnix/modules/services/networking.nix
@@ -8,7 +8,27 @@
   # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
   # Enable networking
 
-  networking.networkmanager.enable = true;
+  networking.networkmanager = {
+    enable = true;
+    dns = "systemd-resolved";
+  };
+
+  services.resolved = {
+    enable = true;
+
+    settings.Resolve = {
+      DNS = [
+        "45.90.28.0#b61c7e.dns.nextdns.io"
+        "45.90.30.0#b61c7e.dns.nextdns.io"
+        "2a07:a8c0::#b61c7e.dns.nextdns.io"
+        "2a07:a8c1::#b61c7e.dns.nextdns.io"
+      ];
+
+      DNSOverTLS = true;
+      Domains = [ "~." ];
+      FallbackDNS = [ ];
+    };
+  };
 
   # Open ports in the firewall.
   # networking.firewall.allowedTCPPorts = [ ... ];
