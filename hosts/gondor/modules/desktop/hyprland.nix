2d01950acb7da74e8c8121a3f8e0c1d9922d7bd5
JoaoSant7 <pedro_romano7@proton.me>
2026-08-04 16:54:12 -0300

    remove dconf for gtk themes

--- hosts/gondor/modules/desktop/hyprland.nix
+++ hosts/gondor/modules/desktop/hyprland.nix
@@ -1,4 +1,4 @@
-{ ... }:
+{ lib, ... }:
 
 {
   # Enable Hyprland with UWSM for proper session registration.
@@ -18,20 +18,4 @@
     trusted-public-keys = [ "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc=" ];
   };
 
-  # Fix issues with gtk themes.
-  programs.dconf.profiles.user.databases = [
-    {
-      settings."org/gnome/desktop/interface" = {
-        gtk-theme = "adw-gtk3-dark";
-        icon-theme = "Adwaita";
-
-        cursor-theme = "Bibata-Modern-Ice";
-        cursor-size = 24;
-
-        font-name = "Rubik Regular 12";
-        document-font-name = "Rubik Regular 12";
-        monospace-font-name = "JetBrainsMono Nerd Font 12";
-      };
-    }
-  ];
 }
